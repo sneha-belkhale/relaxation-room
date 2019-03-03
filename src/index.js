d81791d8f73a83792d1402ec4768ab52b26bd5db
Kirill Kovalevskiy <kovalewskiy@gmail.com>
2019-03-03 11:43:06 +0700

    Added play button

--- src/index.js
+++ src/index.js
@@ -2,4 +2,17 @@ import initWebScene from './PoolScene'
 
 document.body.style.margin = "0";
 
+const playButton = document.getElementById("playButton");
+
+playButton.addEventListener('click', (event) => {
+  const audio = event.target.children[0];
+  if (audio.paused) {
+    audio.play();
+    event.target.classList.add("playing");
+  } else {
+    audio.pause();
+    event.target.classList.remove("playing");
+  }
+})
+
 initWebScene();
